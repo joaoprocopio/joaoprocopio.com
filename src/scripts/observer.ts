3efec098f36d80456f4319d5edd5520b225e16ab
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-02-14 23:00:32 -0300

    funciona

--- src/scripts/observer.ts
+++ src/scripts/observer.ts
@@ -7,13 +7,23 @@ let lastLinkEl: HTMLElement | undefined = undefined
 for (let sectionIndex = 0; sectionIndex < SECTIONS.length; sectionIndex++) {
   const section = SECTIONS[sectionIndex]
 
-  const sectionEl: HTMLElement = document.getElementById(section.ID)!
   const linkEl: HTMLElement = document.getElementById(section.LINK_ID)!
+  const sectionEl: HTMLElement = document.getElementById(section.ID)!
+  const containerEl: HTMLElement = sectionEl.parentElement!
+
+  const containerPadding: number =
+    parseInt(
+      window
+        .getComputedStyle(containerEl)
+        .getPropertyValue("padding-top")
+        .replace("px", ""),
+    ) * 2
 
   const viewportHeight: number = window.innerHeight
-  const sectionHeight: number = sectionEl.clientHeight
-  const observerThreshold: number = Number(
-    (((sectionHeight - viewportHeight) / viewportHeight) % 1).toFixed(2),
+  const sectionHeight: number = sectionEl.clientHeight + containerPadding
+  const observerThreshold: number = Math.max(
+    0,
+    Math.abs(viewportHeight / sectionHeight) / 2,
   )
 
   const sectionObserver = new IntersectionObserver(
