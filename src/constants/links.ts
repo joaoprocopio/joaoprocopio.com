e08386f94dfd111260ba19f56051b91c2a4584a3
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-02-20 03:35:09 -0300

    vai

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,25 +1,21 @@
-import GitHub from "~/icons/github.astro"
-import Gmail from "~/icons/gmail.astro"
-import LinkedIn from "~/icons/linkedin.astro"
-
 export type TLink = {
+  ID: string
   HREF: string
-  ICON: (props: astroHTML.JSX.SVGAttributes) => astroHTML.JSX.Element
 }
 
 export const GITHUB_LINK = {
+  ID: "github",
   HREF: "https://github.com/joaoprocopio",
-  ICON: GitHub,
 } as const satisfies TLink
 
 export const LINKEDIN_LINK = {
+  ID: "linkedin",
   HREF: "https://linkedin.com/in/joao-procopio",
-  ICON: LinkedIn,
 } as const satisfies TLink
 
 export const EMAIL_LINK = {
+  ID: "gmail",
   HREF: "mailto:joaovitorcprocopio@gmail.com",
-  ICON: Gmail,
 } as const satisfies TLink
 
 export const LINKS = [
