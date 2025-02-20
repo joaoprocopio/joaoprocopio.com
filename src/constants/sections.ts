2481adf30a3bd86b2a8cd3fadb027b3526541e58
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-02-20 01:57:57 -0300

    entitled

--- src/constants/sections.ts
+++ src/constants/sections.ts
@@ -2,35 +2,35 @@ export type TSection<S extends string> = {
   ID: S
   LINK_ID: `@${S}`
   HREF: `#${S}`
-  LABEL: Capitalize<S>
+  TITLE: Capitalize<S>
 }
 
 export const SECTION_ABOUT = {
   ID: "about",
   LINK_ID: "@about",
   HREF: "#about",
-  LABEL: "About",
+  TITLE: "About",
 } as const satisfies TSection<"about">
 
 export const SECTION_EXPERIENCE = {
   ID: "experience",
   LINK_ID: "@experience",
   HREF: "#experience",
-  LABEL: "Experience",
+  TITLE: "Experience",
 } as const satisfies TSection<"experience">
 
 export const SECTION_PROJECTS = {
   ID: "projects",
   LINK_ID: "@projects",
   HREF: "#projects",
-  LABEL: "Projects",
+  TITLE: "Projects",
 } as const satisfies TSection<"projects">
 
 export const SECTION_CONTACTS = {
   ID: "contacts",
   LINK_ID: "@contacts",
   HREF: "#contacts",
-  LABEL: "Contacts",
+  TITLE: "Contacts",
 } as const satisfies TSection<"contacts">
 
 export const SECTIONS = [
