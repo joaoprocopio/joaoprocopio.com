9b60939f3cc12257e9c4ae8aa3258c604a37c089
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-05-24 23:28:20 -0300

    iife + microtask

--- src/scripts/observer.ts
+++ src/scripts/observer.ts
@@ -1,64 +1,66 @@
 import { SECTIONS } from "~/constants/sections"
 import { debounce } from "~/utils/debounce"
+;(async () => {
+  const EL_DATA_ACTIVE = "data-active"
+  let observers: IntersectionObserver[] = []
 
-const EL_DATA_ACTIVE = "data-active"
+  const observe = () => {
+    for (
+      let observerIndex = 0;
+      observerIndex < observers.length;
+      observerIndex++
+    ) {
+      const observer = observers[observerIndex]
+      observer.disconnect()
+    }
 
-let observers: IntersectionObserver[] = []
+    observers = []
 
-const observe = () => {
-  for (
-    let observerIndex = 0;
-    observerIndex < observers.length;
-    observerIndex++
-  ) {
-    const observer = observers[observerIndex]
-    observer.disconnect()
+    let prevLinkEl: HTMLElement | undefined = undefined
+
+    for (let sectionIndex = 0; sectionIndex < SECTIONS.length; sectionIndex++) {
+      const section = SECTIONS[sectionIndex]
+
+      const linkEl: HTMLElement = document.getElementById(section.LINK_ID)!
+      const sectionEl: HTMLElement = document.getElementById(section.ID)!
+
+      const viewportHeight: number = window.innerHeight
+      const sectionHeight: number = sectionEl.clientHeight
+
+      const observerThreshold: number =
+        Math.log1p(viewportHeight / sectionHeight) % 1
+
+      const sectionObserver = new IntersectionObserver(
+        (entries) => {
+          const entry = entries[0]
+
+          if (!entry.isIntersecting) {
+            return undefined
+          }
+
+          if (prevLinkEl) {
+            prevLinkEl.setAttribute(
+              EL_DATA_ACTIVE,
+              String(!entry.isIntersecting),
+            )
+          }
+
+          linkEl.setAttribute(EL_DATA_ACTIVE, String(entry.isIntersecting))
+
+          prevLinkEl = linkEl
+        },
+        {
+          threshold: observerThreshold,
+        },
+      )
+
+      sectionObserver.observe(sectionEl)
+
+      observers.push(sectionObserver)
+    }
   }
 
-  observers = []
+  queueMicrotask(observe)
 
-  let prevLinkEl: HTMLElement | undefined = undefined
-
-  for (let sectionIndex = 0; sectionIndex < SECTIONS.length; sectionIndex++) {
-    const section = SECTIONS[sectionIndex]
-
-    const linkEl: HTMLElement = document.getElementById(section.LINK_ID)!
-    const sectionEl: HTMLElement = document.getElementById(section.ID)!
-
-    const viewportHeight: number = window.innerHeight
-    const sectionHeight: number = sectionEl.clientHeight
-
-    const observerThreshold: number =
-      Math.log1p(viewportHeight / sectionHeight) % 1
-
-    const sectionObserver = new IntersectionObserver(
-      (entries) => {
-        const entry = entries[0]
-
-        if (!entry.isIntersecting) {
-          return undefined
-        }
-
-        if (prevLinkEl) {
-          prevLinkEl.setAttribute(EL_DATA_ACTIVE, String(!entry.isIntersecting))
-        }
-
-        linkEl.setAttribute(EL_DATA_ACTIVE, String(entry.isIntersecting))
-
-        prevLinkEl = linkEl
-      },
-      {
-        threshold: observerThreshold,
-      },
-    )
-
-    sectionObserver.observe(sectionEl)
-
-    observers.push(sectionObserver)
-  }
-}
-
-const debouncedObserve = debounce(observe, 100)
-
-window.addEventListener("load", debouncedObserve)
-window.addEventListener("resize", debouncedObserve)
+  window.addEventListener("resize", debounce(observe, 100))
+})()
