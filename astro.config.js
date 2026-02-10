c7f98d720a60c74fd634618b00cf04fb091cc191
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-02-10 17:30:05 +0000

    update

--- astro.config.js
+++ astro.config.js
@@ -1,8 +1,14 @@
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
+import { fileURLToPath, URL } from "node:url"
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "~": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
   },
 })
