8a375a1a5de82101d1acfa5a3e231cd675412f12
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-02-14 02:00:32 +0000

    vai

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import * as compat from "@eslint/compat"
 import js from "@eslint/js"
 import prettier from "eslint-config-prettier/flat"
 import astro from "eslint-plugin-astro"
-import jsxa11y from "eslint-plugin-jsx-a11y"
 import react from "eslint-plugin-react"
 import hooks from "eslint-plugin-react-hooks"
 import * as config from "eslint/config"
@@ -14,17 +13,21 @@ const filename = url.fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 const gitignore = path.resolve(dirname, ".gitignore")
 
-const __eslint_config__ = config.defineConfig(
+const __config = config.defineConfig(
   compat.includeIgnoreFile(gitignore),
   js.configs.recommended,
   tseslint.configs.recommended,
   astro.configs["flat/recommended"],
   astro.configs["flat/jsx-a11y-recommended"],
-  react.configs.flat["recommended"],
-  react.configs.flat["jsx-runtime"],
+  /** @type {import("eslint-plugin-react").ReactFlatConfig} */ (
+    react.configs.flat["recommended"]
+  ),
+  /** @type {import("eslint-plugin-react").ReactFlatConfig} */ (
+    react.configs.flat["jsx-runtime"]
+  ),
   hooks.configs.flat["recommended-latest"],
-  jsxa11y.flatConfigs.recommended,
   prettier,
+  { settings: { react: { version: "detect" } } },
 )
 
-export default __eslint_config__
+export default __config
