383945403ca214756cc6c35b5056205a6b88b4f3
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-02-14 01:53:42 +0000

    vai

--- astro.config.js
+++ astro.config.js
@@ -1,18 +1,22 @@
+import mdx from "@astrojs/mdx"
+import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
+import reactCompiler from "babel-plugin-react-compiler"
 import { fileURLToPath, URL } from "node:url"
-import sitemap from "@astrojs/sitemap"
 
-import react from "@astrojs/react"
-
-import mdx from "@astrojs/mdx"
+/** @type {import("babel-plugin-react-compiler").PluginOptions} */
+const reactCompilerOptions = {
+  target: "19",
+}
 
 export default defineConfig({
   site: "https://joaoprocopio.com",
   integrations: [
     sitemap() /* TODO: integrar o sitemap com o blog  */,
-    react(),
     mdx(),
+    react({ babel: { plugins: [[reactCompiler, reactCompilerOptions]] } }),
   ],
   vite: {
     plugins: [tailwindcss()],
--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,9 @@ import * as compat from "@eslint/compat"
 import js from "@eslint/js"
 import prettier from "eslint-config-prettier/flat"
 import astro from "eslint-plugin-astro"
+import jsxa11y from "eslint-plugin-jsx-a11y"
+import react from "eslint-plugin-react"
+import hooks from "eslint-plugin-react-hooks"
 import * as config from "eslint/config"
 import * as path from "node:path"
 import * as url from "node:url"
@@ -13,51 +16,15 @@ const gitignore = path.resolve(dirname, ".gitignore")
 
 const __eslint_config__ = config.defineConfig(
   compat.includeIgnoreFile(gitignore),
-  {
-    languageOptions: {
-      parserOptions: {
-        project: "./tsconfig.json",
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-    },
-  },
-  withAstroFiles(js.configs.recommended),
-  withAstroFiles(tseslint.configs.recommended),
-  withAstroFiles({
-    languageOptions: {
-      globals: {
-        astroHTML: true,
-      },
-    },
-  }),
-  astro.configs.recommended,
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  astro.configs["flat/recommended"],
+  astro.configs["flat/jsx-a11y-recommended"],
+  react.configs.flat["recommended"],
+  react.configs.flat["jsx-runtime"],
+  hooks.configs.flat["recommended-latest"],
+  jsxa11y.flatConfigs.recommended,
   prettier,
 )
 
 export default __eslint_config__
-
-/**
- * @typedef   {import("typescript-eslint").ConfigArray} ConfigArray
- * @param     {ConfigArray | ConfigArray[number]} config
- * @returns   {ConfigArray | ConfigArray[number]}
- */
-function withAstroFiles(config) {
-  if (Array.isArray(config)) {
-    config?.forEach((config) => {
-      if (!config.files) {
-        return undefined
-      }
-
-      config.files = config.files.concat("**.astro")
-    })
-
-    return config
-  }
-
-  if (!config.files) return config
-
-  config.files = config.files.concat("**.astro")
-
-  return config
-}
