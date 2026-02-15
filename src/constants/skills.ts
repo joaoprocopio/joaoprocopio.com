94dc175ca87f65b03b6dfd37adb7f127b7cf47ff
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-02-15 04:15:39 +0000

    vai

--- src/constants/skills.ts
+++ src/constants/skills.ts
@@ -1,6 +1,7 @@
 export const HARD_SKILLS = {
   // Frameworks
   REACT: "React",
+  REACT_NATIVE: "React Native",
   VUE: "Vue",
   NUXT: "Nuxt",
   DJANGO: "Django",
@@ -18,11 +19,14 @@ export const HARD_SKILLS = {
   SQL: "SQL",
 
   // Libraries and Tools
+  TANSTACK_QUERY: "Tanstack Query",
   MAPBOX: "Mapbox",
   CELERY: "Celery",
   GIT: "Git",
   LINUX: "Linux",
   NGINX: "NGINX",
+  CAPACITOR: "Capacitor",
+  NODE: "Node.js",
 
   // Data
   DBT: "DBT (Data Build Tool)",
