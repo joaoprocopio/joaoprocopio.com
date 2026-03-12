5ca60e11de00cf1d16e52df0a60df0ea3d69d118
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-03-12 17:55:22 +0000

    vai

--- app/components/cube.vue
+++ app/components/cube.vue
@@ -7,19 +7,21 @@ const cubeRef = shallowRef<TresInstance | null>()
 loop.onBeforeRender((context) => {
   if (cubeRef.value) {
     cubeRef.value.position.y = 0.3 * Math.sin(1 * context.elapsed)
-
-    cubeRef.value.rotation.x = context.elapsed * 0.5
-    cubeRef.value.rotation.y = context.elapsed * 0.2
-    cubeRef.value.rotation.z = context.elapsed * 0.1
+    cubeRef.value.rotation.y = context.elapsed * 0.6
+    cubeRef.value.rotation.x = context.elapsed * 0.3
+    cubeRef.value.rotation.z = context.elapsed * 0.2
   }
 })
 </script>
 
 <template>
-  <TresPerspectiveCamera :position="[2, 2, 2]" :look-at="[0, 0, 0]" />
+  <TresPerspectiveCamera :position="[2, 1, 3]" :look-at="[0, 0, 0]" />
 
   <TresMesh ref="cubeRef" :position="[0, 0, 0]">
     <TresBoxGeometry />
     <TresMeshBasicMaterial />
   </TresMesh>
+
+  <TresAxesHelper />
+  <TresGridHelper />
 </template>
